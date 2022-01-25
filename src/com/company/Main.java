3654b52cc547488ac847d671e8b392fe5495f2ce
AnkitKumar <ankit.kumar@infostride.com>
2022-01-25 17:50:41 +0530

    Wrote a program on collection on arraylist.

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> a1 = new ArrayList<Integer>();
+        for(int i=1;i<=5;i++)
+            a1.add(i);
+
+        System.out.println(a1);
+        a1.remove(3);
+        System.out.println(a1);
+        for(int i=0;i< a1.size();i++)
+            System.out.print(a1.get(i) + " ");
+
+
+    }
+}
